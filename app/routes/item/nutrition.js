5becfe3cb8a50a5ee8678919431558468839b6a6
rishabmps <rishab.mps@gmail.com>
2016-09-30 21:03:57 +0530

    Model fetching for nested routes

--- app/routes/item/nutrition.js
+++ app/routes/item/nutrition.js
@@ -1,5 +1,9 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
-	return Ember.$.get('/menu/'+params.item_name+'.json');
+	model(){
+		
+		return this.modelFor('item');
+	}
+	
 });
